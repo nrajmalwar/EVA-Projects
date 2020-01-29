93f9e8b6debdd06f92edbf5872efe279c7cfef9b
nrajmalwar <47084480+nrajmalwar@users.noreply.github.com>
2020-01-29 11:31:43 +0530

    Update data.py

--- Session_15/data/data.py
+++ Session_15/data/data.py
@@ -29,12 +29,12 @@ def pad4(x):
   pad4 = lambda x: np.pad(x, [(0, 0), (4, 4), (4, 4), (0, 0)], mode='reflect')
   x = pad4(x)
 
-  return x 
+  return x
 
 def Cutout(x_train, y_train, num_images):
   """Function to add cutout augmentation"""
   def Custom_Aug(img):
-  seq = Compose([
+      seq = Compose([
           Cutout(num_holes=1, p=0.5)], p=1)
   return seq(image=img)['image']
 
@@ -58,7 +58,7 @@ def plot_image(x):
 
 def _bytes_feature(value):
   """Returns a bytes_list from a string / byte."""
-  
+
   if isinstance(value, type(tf.constant(0))):
     value = value.numpy()
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -87,12 +87,12 @@ def convert_proto_message_to_np(protocol_message):
 
     parsed_out = tf.io.parse_single_example(serialized=protocol_message,
                                             features=feature_dict)
-    
+
     image = tf.io.decode_raw(parsed_out['image'], out_type=tf.float32)
     label = parsed_out['label']
 
     return (image,label)
-    
+
 def reshape_img(X, img_shape):
     img = X[0].numpy()
     label = X[1]
@@ -100,12 +100,12 @@ def reshape_img(X, img_shape):
     img_tensor = tf.convert_to_tensor(img)
 
     return (img_tensor, label)
-  
-def convert_np_to_tfrecords(images, labels, 
+
+def convert_np_to_tfrecords(images, labels,
                             batch_size=128,filename=None):
 
     # Converts a numpy array into TFReocrds
-    
+
     channels = images.shape[-1]
     width = images.shape[-2]
     height = images.shape[-3]
@@ -118,9 +118,9 @@ def convert_np_to_tfrecords(images, labels,
             protocol_message = convert_np_to_tf_example(img,label)
 
             writer.write(protocol_message)
-        
+
     dataset = tf.data.TFRecordDataset(filename)
     dataset = dataset.map(convert_proto_message_to_np)
     dataset = dataset.map(lambda x, y: (tf.reshape(x,img_shape), y))
-    
+
     return dataset
